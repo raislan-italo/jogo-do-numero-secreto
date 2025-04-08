11f9ea51a03bd2b862b084463bf6de6a3f239809
Raislan-Italo <raislanitalo13@gmail.com>
2025-04-07 23:10:11 -0300

    Adicionado responsividade

--- app.js
+++ app.js
@@ -1,71 +1,73 @@
+// Declaração das variáveis que serão usadas futuramente
 let listaDeNumerosSorteados = [];
 let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1;
+let qtTentativas = 1;
 
-// Função que exibe um texto na tela
-// Parâmetros: tag: a ser exibida o texto --- texto: texto a ser inserido na tag
+// Função usada para exibir o texto no display
 function exibirTextoNaTela(tag, texto) {
-   let campo = document.querySelector(tag);
-   campo.innerHTML = texto;
-   responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    let campo = document.querySelector(tag);    // Obtem a tag passada por parâmetro
+    campo.innerHTML = texto;    // Adiciona o texto na tag obtida
 }
 
-function mensagemInicial() {
-   exibirTextoNaTela('h1', 'Jogo do número secreto');
-   exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');   
+// Função usada para exibir o texto inicial no display
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
-mensagemInicial();
+// Chama a função para exibir o texto inicial
+exibirMensagemInicial();
 
-// Função em JavaScript: Usada para criar um bloco de cógido específico
+// Função usada para verificar o valor escolhido pelo usuário
 function verificarChute() {
-   let chute = document.querySelector('input').value;
-   
-   if (chute == numeroSecreto) {
-      exibirTextoNaTela('h1', 'Acertou');
-      let palavraTentativa = (tentativas > 1) ? 'tentativas' : 'tentativa';
-      let mensagemTentativa = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
-      exibirTextoNaTela('p', mensagemTentativa);
-      document.getElementById('reiniciar').removeAttribute('disabled');
-   } else {
-      if (chute > numeroSecreto) {
-         exibirTextoNaTela('p', 'O número secreto é menor');
-      } else {
-         exibirTextoNaTela('p', 'O número secreto é maior');
-      }
-      tentativas++;
-      limparCampo();
-   }
-
+    let chute = document.querySelector('input').value;  // Obtém o dado do campo input
+    
+    // Verifica se o chute é igual ao número secreto
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = qtTentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${qtTentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        qtTentativas++;
+        limparCampo();
+    }
 }
 
+// Função usada para gerar números aleatórios e armazenar em uma lista
 function gerarNumeroAleatorio() {
-   let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
-   let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
 
-   if (quantidadeDeElementosNaLista == numeroEscolhido) {
-      listaDeNumerosSorteados = [];
-   }
-
-   if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
-      return gerarNumeroAleatorio();
-   } else {
-      listaDeNumerosSorteados.push(numeroEscolhido);
-      console.log(listaDeNumerosSorteados);
-      return numeroEscolhido;
-   }
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 }
 
+// Função usada para limpar o campo de entrada
 function limparCampo() {
-   chute = document.querySelector('input');
-   chute.value = '';
+    chute = document.querySelector('input');
+    chute.value = '';
 }
 
+// Função usada para reiniciar o jogo
 function reiniciarJogo() {
-   numeroSecreto = gerarNumeroAleatorio();
-   limparCampo();
-   tentativas = 1;
-   mensagemInicial();
-   document.getElementById('reiniciar').setAttribute('disabled', true);
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    qtTentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
